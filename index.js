8a3c0c904d90815806075199cfcd197166373a8b
Rupam Mukherjee <rupam15497@gmail.com>
2019-05-05 14:55:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
 
-function destructivelyAppendKitten(array, ele)
+function destructivelyAppendKitten(ele)
 {
+  var array = new Array()
   array = [...array, ele]
   return array
 }
 
-function destructivelyPrependKitten(array, ele)
+function destructivelyPrependKitten(ele)
 {
   array = [ele, ...array]
   return array
 }
 
-function destructivelyRemoveLastKitten(array) {
+function destructivelyRemoveLastKitten() {
   array = array.slice(0, array.length-1)
   return array
 }
 
-function destructivelyRemoveFirstKitten(array){
+function destructivelyRemoveFirstKitten(){
   array = array.slice(1)
   return array
 }
